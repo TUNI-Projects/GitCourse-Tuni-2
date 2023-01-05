3138b89fa71d989470850c45a331a08115b5c4cd
Ibtehaz <10481417+shepherd-06@users.noreply.github.com>
2023-01-05 16:35:41 +0200

    merge sort updated

--- basics/mergesort.py
+++ basics/mergesort.py
@@ -5,8 +5,14 @@ def debug_print(debug_msg=None, **kwargs):
         print(debug_msg)
 
     for key, value in kwargs.items():
-        print("{}:
-              {}".format(key, value))
+        print("{}:{}".format(key, value))
+
+
+def mergesort(array):
+    debug_print(array=array)
+    if len(array) <= 1:
+        return array
+
 
 def mergesort(array):
     debug_print(array=array)
@@ -60,18 +66,3 @@ if __name__ == "__main__":
 
     sorted_list = mergesort(value_list)
     print(sorted_list)
-if __name__ == "__main__":
-    input_str = input("Enter numbers, separated by ',': ")
-
-    input_list = input_str.split(",")
-    debug_print(input_list=input_list)
-
-    value_list = []
-    for x in input_list:
-        try:
-            value_list.append(int(x))
-        except ValueError as err:
-            print("Invalid input.")
-            quit(1)
-
-    debug_print(value_list=value_list)
